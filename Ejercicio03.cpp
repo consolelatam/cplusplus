d7df4bdf62669e9308b9cb373b53d886858cf6e1
console <61861945+consolelatam@users.noreply.github.com>
2020-03-12 22:55:15 -0600

    Solución ejercicio 3
    Mostrar los primeros 100 números naturales en orden descendiente.

---
+++ Ejercicio03.cpp
@@ -0,0 +1,15 @@
+/*
+    Mostrar los primeros 100 numeros naturales en orden
+    descendiente.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 100; i > 0; i--) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
