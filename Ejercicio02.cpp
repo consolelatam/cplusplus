8d73df811b1452bdf571c84dbff5d6723ba6a155
console <61861945+consolelatam@users.noreply.github.com>
2020-03-12 22:48:25 -0600

    Solución ejercicio 2
    Mostrar en pantalla los primeros 100 números naturales en orden ascendente.

---
+++ Ejercicio02.cpp
@@ -0,0 +1,15 @@
+/*
+    Mostrar en pantalla los primeros 100 numeros naturales
+    en orden ascendente.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
